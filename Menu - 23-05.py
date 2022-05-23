a765b9248c74f01bac34a172883d89eac2819bb7
Marchiello <99300987+Marchiello@users.noreply.github.com>
2022-05-23 10:49:10 -0300

    Add files via upload

---
+++ Menu - 23-05.py
@@ -0,0 +1,15 @@
+opcao = 1
+while opcao != 4:
+    print("1 - Opção 1")
+    print("2 - Opção 2")
+    print("3 - Opção 3")
+    print("4 - Sair")
+    opcao = int(input("Escolha uma opção:\n"))
+    if opcao==1:
+        print('1 - Opção 1')
+    elif opcao==2:
+        print("2 - Opção 2")
+    elif opcao==3:
+        print("3 - Opção 3")
+    else:
+        print("SAIR")
